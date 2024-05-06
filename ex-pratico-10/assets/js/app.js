362473576cf5516f34611b9a3ce0994a2145c744
Otavio Chaves <otaviocs2005@gmail.com>
2024-05-06 00:28:39 -0300

    Html css finalizados

---
+++ ex-pratico-10/assets/js/app.js
@@ -0,0 +1,12 @@
+const currencys = [
+    { name: "real", surname: "BRL", symbol: "R$", rates: { real: 1.00, dolar: 0.20, euro: 0.18, yuan: 1.42, peso: 171.73 } },
+    { name: "dolar", surname: "USD", symbol: "$", rates: { real: 5.10, dolar: 1.00, euro: 0.93, yuan: 7.24, peso: 876.14 } },
+    { name: "euro", surname: "EUR", symbol: "€", rates: { real: 6.40, dolar: 1.08, euro: 1.00, yuan: 7.79, peso: 943.54 } },
+    { name: "yuan", surname: "CNY", symbol: "¥", rates: { real: 0.70, dolar: 0.14, euro: 0.13, yuan: 1.00, peso: 121.05 } },
+    { name: "peso", surname: "ARS", symbol: "$", rates: { real: 0.0058, dolar: 0.0011, euro: 0.0011, yuan: 0.0083, peso: 1.00 } }
+]
+
+function changeSelect() {
+
+}
+
